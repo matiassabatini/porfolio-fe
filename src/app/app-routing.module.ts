40984c0fdf6dc552c1e3e64b7468a98f00b3e63d
matiassabatini <matiassabatini1998@gmail.com>
2023-11-04 01:45:26 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // Ruta raíz
+    redirectTo: '/home', // Redirigir a la ruta '/home'
+    pathMatch: 'full'
+  },
+  {
+    
+    path:'home',
+    component:HomeComponent,
+    pathMatch:'full'
+
+  },
+  {
+    
+    path:'proyectos',
+    component:ProyectosComponent,
+    pathMatch:'full'
+
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './pages/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
